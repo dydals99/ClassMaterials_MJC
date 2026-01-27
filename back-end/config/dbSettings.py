0906044c485b01611aafe5658d195c3c8ac59e49
dydals99 <whdydals8604@gmail.com>
2026-01-27 15:12:24 +0900

    update : 데이터베이스 세팅

---
+++ back-end/config/dbSettings.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
